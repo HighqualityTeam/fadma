ffcfed5600b073e0294016d755e2ec8d0a5f2eb4
HighqualityTeam <120262621+HighqualityTeam@users.noreply.github.com>
2023-02-11 15:34:01 +0100

    Add files via upload

---
+++ anas.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+// even and odd numbers 
+// n%2==0 / n even / odd
+
+
+int main()
+{
+    while(true) {
+        int n;
+   cout<<"Please Enter num: \n";
+   cin>>n;
+   if(n%2==0) 
+   cout<<"is even "<<endl;
+  
+   else
+   cout<<"is odd"<<endl;
+    }
+   
+return 0;
+}
